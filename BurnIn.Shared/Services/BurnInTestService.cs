78cd224d747a0f9c074dc9a9cd8571accc8f2a8b
Andrew Elmendorf <aelmendorf@s-et.com>
2024-02-03 10:13:00 -0500

    one more

--- BurnIn.Shared/Services/BurnInTestService.cs
+++ BurnIn.Shared/Services/BurnInTestService.cs
@@ -6,7 +6,7 @@ namespace BurnIn.Shared.Services;
 public class BurnInTestService {
     public event EventHandler TestStartedHandler;
     public event EventHandler TestCompleteHandler;
-    //last check
+    //last check check
     
     private StationSerialData _latestData;
     private BurnInTestLog _runningTest=new BurnInTestLog();

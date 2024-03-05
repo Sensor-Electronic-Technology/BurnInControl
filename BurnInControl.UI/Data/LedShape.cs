aa96ae655b1fb02d698d7a810984927d23c1a574
Andrew Elmendorf <aelmendorf@s-et.com>
2024-03-05 07:01:52 -0500

    UI and refactoring

---
+++ BurnInControl.UI/Data/LedShape.cs
@@ -0,0 +1,9 @@
+﻿using Ardalis.SmartEnum;
+namespace BurnInControl.UI.Data;
+
+public class LedShape : SmartEnum<LedShape,string>{
+    public static readonly LedShape LedCircle=new LedShape(nameof(LedCircle), "led");
+    public static readonly LedShape LedRect=new LedShape(nameof(LedRect), "led-rectangle");
+
+    public LedShape(String name, String value) : base(name, value) {  }
+}
---
+++ BurnInControl.UI/Services/StationStreaming.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR.Client;
+namespace BurnInControl.UI.Services;
+
+public class StationStreaming:BackgroundService,IAsyncDisposable {
+    /*private readonly ILogger<AmmoniaHubService> _logger;
+    private readonly IHubContext<StationHub, ISendTankWeightsCommand> _hubContext;*/
+    private HubConnection? _hubConnection;
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken) {
+        throw new NotImplementedException();
+    }
+    public ValueTask DisposeAsync() {
+        throw new NotImplementedException();
+    }
+}

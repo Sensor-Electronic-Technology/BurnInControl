8411adaed812ca636c7e630ca9887905ff3fa16b
Andrew Elmendorf <aelmendorf234@gmail.com>
2024-03-06 11:36:33 -0500

    UI Configuration Work

--- BurnInControl.ConsoleTesting/Program.cs
+++ BurnInControl.ConsoleTesting/Program.cs
@@ -2,4 +2,8 @@
 
 var numberstr="123.434e-3";
 var number=double.Parse(numberstr);
+
+var digitialString = "D55";
+var digitalNumber = int.Parse(digitialString.Substring(1));
 Console.WriteLine("Number: "+number);
+Console.WriteLine("Digital Number: "+digitalNumber);
---
+++ BurnInControl.Data/StationModel/ControllerHardwareConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace BurnInControl.Data.StationModel;
+
+public static class ControllerHardwareConstants {
+    public static readonly int PROBE_COUNT = 6;
+    public static readonly int HEATER_COUNT = 3;
+    public static readonly int NTC_COUNT = 3;
+    public static readonly int DIGITAL_COUNT = 53;
+    public static readonly int ANALOG_COUNT = 16;
+}

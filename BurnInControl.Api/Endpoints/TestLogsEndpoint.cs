60b61f4a80dde8d87d1f3fe7d5943f64693057f7
Andrew Elmendorf <aelmendorf@s-et.com>
2024-03-13 06:52:53 -0400

    web api template

---
+++ BurnInControl.Api/Endpoints/TestLogsEndpoint.cs
@@ -0,0 +1,28 @@
+﻿using BurnInControl.Data.BurnInTests;
+using FastEndpoints;
+using MongoDB.Driver;
+namespace BurnInControl.Api.Endpoints;
+
+public class TestLogsEndpoint:Endpoint<EmptyRequest,BurnInTestLog> {
+    private readonly IMongoCollection<BurnInTestLog> _logsCollection;
+    public TestLogsEndpoint(IMongoClient client) {
+        var database = client.GetDatabase("burn_in_db");
+        this._logsCollection= database.GetCollection<BurnInTestLog>("test_logs");
+    }
+    
+    public override void Configure() {
+        Get("/api/testlogs");
+        AllowAnonymous();
+    }
+    
+    public override async Task HandleAsync(EmptyRequest request, CancellationToken cancellationToken) {
+        var log = await this._logsCollection.Find(_ => true).FirstOrDefaultAsync(cancellationToken);
+        if (log != null) {
+            await SendAsync(log,cancellation: cancellationToken);
+        }else {
+            await SendNotFoundAsync(cancellationToken);
+        }
+        
+    }
+    
+}

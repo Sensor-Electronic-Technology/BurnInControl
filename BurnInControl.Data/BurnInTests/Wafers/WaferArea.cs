0dc6d1a7279925a4340c1fdadc6062ffbdff0066
Andrew Elmendorf <aelmendorf@s-et.com>
2024-03-20 17:00:58 -0400

    finalize seperation of QuickTest data.  Moved to nuget

--- BurnInControl.Data/BurnInTests/Wafers/WaferArea.cs
+++
@@ -1,9 +0,0 @@
-using Ardalis.SmartEnum;
-namespace BurnInControl.Data.BurnInTests.Wafers;
-
-public class WaferArea : SmartEnum<WaferArea, string> {
-    public static WaferArea Center = new WaferArea(nameof(Center), "C");
-    public static WaferArea Middle = new WaferArea(nameof(Middle), "M");
-    public static WaferArea Edge = new WaferArea(nameof(Edge), "E");
-    public WaferArea(String name, String value) : base(name, value) { }
-}

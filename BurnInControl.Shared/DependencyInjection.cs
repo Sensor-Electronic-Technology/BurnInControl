ba3c95fa13bbbb0dedd74ce7c8650c07af9bd322
Andrew Elmendorf <aelmendorf@s-et.com>
2024-04-21 16:11:41 -0400

    update station-service entrypoint with curl and arduino cli install

--- BurnInControl.Shared/DependencyInjection.cs
+++ BurnInControl.Shared/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using BurnInControl.Shared.AppSettings;
+﻿using System.Runtime.InteropServices;
+using BurnInControl.Shared.AppSettings;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 namespace BurnInControl.Shared;
@@ -7,6 +8,11 @@ public static class DependencyInjection {
     public static IServiceCollection AddSettings(this IServiceCollection services, IHostApplicationBuilder builder) {
         services.Configure<StationSettings>(builder.Configuration.GetSection(nameof(StationSettings)));
         services.Configure<DatabaseSettings>(builder.Configuration.GetSection(nameof(DatabaseSettings)));
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
+            
+        } else {
+            
+        }
         services.Configure<FirmwareUpdateSettings>(builder.Configuration.GetSection(nameof(FirmwareUpdateSettings)));
         return services;
     }
--- BurnInControl.StationService/entry-script.sh
+++ BurnInControl.StationService/entry-script.sh
@@ -1,3 +1,5 @@
 ﻿#!/bin/sh
 apk add gcompat
+apk add curl
+curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | sh
 dotnet /app/BurnInControl.StationService.dll

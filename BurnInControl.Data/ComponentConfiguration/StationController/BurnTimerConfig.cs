a284e410f7f319d6f03bc8fc80f146b48968fcea
aelmendorf234 <aelmendorf234@gmail.com>
2024-06-11 09:15:54 -0400

    add timeoff config changed

--- BurnInControl.Data/ComponentConfiguration/StationController/BurnTimerConfig.cs
+++ BurnInControl.Data/ComponentConfiguration/StationController/BurnTimerConfig.cs
@@ -4,10 +4,13 @@ public class BurnTimerConfig {
     public ulong Time60mASec { get; set; }
     public ulong Time120mASec { get; set; }
     public ulong Time150mASec { get; set; }
-    public BurnTimerConfig(ulong t60, ulong t120, ulong t150) {
+    public double TimeOffPercent { get; set; }
+    
+    public BurnTimerConfig(ulong t60, ulong t120, ulong t150,double offPercent) {
         this.Time60mASec = t60;
         this.Time120mASec = t120;
         this.Time150mASec = t150;
+        this.TimeOffPercent= offPercent;
         
     }
 
@@ -15,5 +18,6 @@ public class BurnTimerConfig {
         this.Time60mASec = 72000;
         this.Time120mASec = 72000;
         this.Time150mASec = 25200;
+        this.TimeOffPercent = 95.0;
     }
 }

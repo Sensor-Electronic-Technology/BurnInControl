d29bf3d7dd293a1f4e841d8b23d5ebed0ee85cd0
aelmendorf234 <aelmendorf234@gmail.com>
2024-06-22 11:56:12 -0400

    add systemd to build pipeline

--- BurnInControl.HostRunner/Program.cs
+++ BurnInControl.HostRunner/Program.cs
@@ -3,6 +3,7 @@ using BurnInControl.HostRunner.Hubs;
 using Docker.DotNet;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseSystemd();
 builder.Services.AddHostedService<Worker>();
 builder.Services.AddSignalR(options => { 
     options.EnableDetailedErrors = true;
@@ -11,6 +12,7 @@ builder.Services.AddSingleton<IDockerClient>(new DockerClientConfiguration(
         new Uri("unix:///var/run/docker.sock"))
     .CreateClient());
 var host = builder.Build();
+
 host.Urls.Add("http://localhost:4000");
 host.MapHub<HostHub>("/hubs/host");
 host.Run();

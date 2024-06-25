523815d4df5f12893ce3c0b6104ad6c16c34929d
aelmendorf234 <aelmendorf234@gmail.com>
2024-06-25 16:35:58 -0400

    fix DependencyInjection.cs, remove asp

--- BurnInControl.Shared/DependencyInjection.cs
+++ BurnInControl.Shared/DependencyInjection.cs
@@ -1,6 +1,4 @@
-﻿using System.Runtime.InteropServices;
-using BurnInControl.Shared.AppSettings;
-using Microsoft.AspNetCore.Builder;
+﻿using BurnInControl.Shared.AppSettings;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 namespace BurnInControl.Shared;

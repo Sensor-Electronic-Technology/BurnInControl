fe1b93f4153e8f128b04a7688dd6826c147e790e
aelmendorf234 <aelmendorf234@gmail.com>
2024-07-24 14:36:27 -0400

    fix probe to pocket number and added alert styles for probes that are not loaded

--- BurnInControl.Dashboard/Data/ProbeAlert.cs
+++ BurnInControl.Dashboard/Data/ProbeAlert.cs
@@ -2,14 +2,14 @@ namespace BurnInControl.Dashboard.Data;
 
 public class ProbeAlert {
     public string ProbeId { get; set; }
-    public bool Latched { get; set; }
+    public bool Loaded { get; set; }
     public bool Okay { get; set; }
     public bool Acknowledged { get; set; }
     
     public ProbeAlert(int index) {
         this.ProbeId = $"P{index+1}";
         this.Acknowledged = false;
-        this.Latched = false;
+        this.Loaded = false;
         this.Okay = true;
     }
 }
